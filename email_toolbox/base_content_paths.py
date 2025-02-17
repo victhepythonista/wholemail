693cbce49e4456a1e16cab2086769fbeb92c6b1c
victhepythonista <letingvictorkipkemboi@gmail.com>
2025-02-17 21:27:05 +0300

    Gitignored oldcode folder

--- email_toolbox/base_content_paths.py
+++
@@ -1,5 +0,0 @@
-from os.path import join 
-
-base_dir = 'eqp/email_toolbox/email_templates'
-verify_code = join( base_dir, 'base_verify_code_email.html')
-subject_message = join(base_dir, 'base_subject_message_email.html')
--- email_toolbox/body_content_paths.py
+++
@@ -1,5 +0,0 @@
-from os.path import join 
-
-contents_dir = 'eqp/email_toolbox/email_templates/contents'
-verify_code = join( contents_dir , 'verify_code.html')
-subject_message =  join( contents_dir , 'subject_message.html')
